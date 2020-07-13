a2af97f90167638125916d7ae0c653c6b2b9c88c
Keenan Jones <keenanjones@keenans-air.attlocal.net>
2020-07-12 19:56:13 -0500

    Signin error handling

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -1,14 +1,14 @@
 class AuthController < ApplicationController
   def create
     user = User.find_by(email: params["email"])
-    # byebug
-    payload = { user_id: user.id }
-    token = JWT.encode(payload, 'tweak', 'HS256')
-
+    
+    
     if user && user.authenticate(params[:password])
+      payload = { user_id: user.id }
+      token = JWT.encode(payload, 'tweak', 'HS256')
       render json: {token: token}
     else
-      render json: { error: 'User not found' }
+      render json: { error: 'User not found' }, status: 404
     end
 
   end
@@ -18,12 +18,11 @@ class AuthController < ApplicationController
     decoded_token = JWT.decode(token, 'tweak', true, algorithm: 'HS256')
     user_id = decoded_token[0]['user_id']
     user = User.find(user_id)
-
     if user 
       render json: UserSerializer.new(user).to_serialized_json
-  else
+    else
       render json: {error: 'Invalid Token'}, status: 401
-  end
+    end
   end
 
 end
